bc22324d7b465f8a212f69361a972e6494ff060e
dictateurfou <stevenhervy@gmail.com>
2019-04-18 09:06:29 +0200

    init with lexik jwt

---
+++ src/Controller/ApiController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use App\Entity\User;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+/**
+ * @Route("/api", name="api_")
+ */
+class ApiController extends AbstractController
+{
+    /**
+    * @Route("/register", name="register")
+    */
+    public function register(Request $request, UserPasswordEncoderInterface $encoder)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $username = $request->request->get('_username');
+        $password = $request->request->get('_password');
+        $user = new User($username);
+        $user->setPassword($encoder->encodePassword($user, $password));
+        $em->persist($user);
+        $em->flush();
+        return new Response(sprintf('User %s successfully created', $user->getUsername()));
+    }
+
+    /**
+    * @Route("/test", name="test")
+    */
+
+    public function test(Request $request)
+    {
+        return new Response(sprintf('Logged in as %s', $this->getUser()->getUsername()));
+    }
+
+
+}
